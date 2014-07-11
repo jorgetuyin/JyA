04a3dc8ad227dd9104d551714277658e5dcd94ab
jorgetuyin <jorge_tuyin@outlook.com>
2014-07-11 01:40:29 -0500

    Juega y Aprende

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Juega y Aprende</title>
+  <base href="http://localhost/a/"/>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+ </head>
+  <body>
+  <div class="container nav-row" style="margin-top:50px;">
+    <div class=""><img src="images/bunny.png"/></div>
+  <div class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="images/Logo_C.png"/></a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+             <li class="active"><a href="#" class="red"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
+               <li class=""><a href="#" class="orange"><span class="glyphicon glyphicon-tower"></span> Juegos</a></li>
+			   <li class=""><a href="#" class="green"><span class="glyphicon glyphicon-star"></span> Actividades</a></li>
+			   <li class=""><a href="#" class="blue"><span class="glyphicon glyphicon-heart"></span> Entretenimiento</a></li>
+              <li class=""><a href="#"class="pink"><span class="glyphicon glyphicon-film"></span> Videos</a></li>              
+			  <li class=""><a href="#"class="purple"><span class="glyphicon glyphicon-plus"></span> Mas</a></li>			  
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+          <a href="#" class="dropdown-toggle orange" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Padres <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </div>
+ </div>
+  <!-- other-->
+  <div class="container">
+<div class="col-lg-12 col-sm-6 visible-lg visible-md">
+<div class="parallax" style=" padding-top: 300px"> 
+ Aqui va Slider
+</div>
+<br/> 
+</div>  
+ 
+<div class="col-lg-9" >
+<div style="background-color: #FFF; padding-top: 350px"> 
+ Aqui va algo 1
+</div>
+<br/> 
+</div>
+
+<div class="col-lg-3">
+<div style="background-color: #FFF; padding-top: 350px"> 
+Aqui va otro 2
+</div>
+<br/> 
+</div>
+
+<div class="col-lg-3">
+<div style="background-color: #FFF; padding-top: 350px"> 
+ Aqui va algo 3
+</div>
+<br/> 
+</div>
+
+<div class="col-lg-9">
+<div style="background-color: #FFF; padding-top: 350px"> 
+Aqui va otro 4
+</div>
+<br/> 
+</div>
+<!-- end container-->
+</div>	
+	<!-- END -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+     <script src="js/jquery-1.10.2.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+	<script src="js/custom.js"></script>
+	<script src="http://mymaplist.com/js/vendor/TweenLite.min.js"></script>
+  </body>
+</html>
